d2ff0de9807ddf937d2ee7181b123e8613b15a7b
Samuel_V <samuel@dropps.io>
2022-08-08 19:55:15 +0100

    feat: profile page integration

--- src/core/api.ts
+++ src/core/api.ts
@@ -3,3 +3,11 @@ import {API_URL} from "../environment/endpoints";
 export async function fetchProfileInfo(address: string) {
   return await (await fetch(API_URL + '/lookso/profile/' + address + '/info')).json();
 }
+
+export async function fetchProfileFollowingCount(address: string): Promise<number> {
+  return (await (await fetch(API_URL + '/lookso/profile/' + address + '/following/count')).json()).following;
+}
+
+export async function fetchProfileFollowersCount(address: string): Promise<number> {
+  return (await (await fetch(API_URL + '/lookso/profile/' + address + '/followers/count')).json()).followers;
+}
