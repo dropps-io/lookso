5dba651a77d6608cb0d79b42eead264c9fe39d32
Samuel_V <samuel@dropps.io>
2022-08-20 18:32:19 +0100

    feat: endpoints changed

--- src/environment/endpoints.ts
+++ src/environment/endpoints.ts
@@ -1,6 +1,6 @@
 export const RPC = 'https://rpc.l16.lukso.network';
 export const IPFS_GATEWAY = 'https://2eff.lukso.dev/ipfs/';
 export const NATIVE_TOKEN = 'LYXt';
-export const API_URL = 'http://127.0.0.1:3030';
+export const API_URL = 'https://api.lookso.io';
 export const EXPLORER_URL = 'https://explorer.execution.l16.lukso.network/';
-export const POST_VALIDATOR_ADDRESS = '0xA5284665954a54d12737Da405824160cCE05B0B0';
+export const POST_VALIDATOR_ADDRESS = '0xe3c6642a58ec9E75F0AB4F3eBbbd140EecaDa177';
