7fd3a0b90d83c1f695f8044a76eafe1beaed09d7
Samuel_V <samuel@dropps.io>
2022-08-21 21:17:39 +0100

    chore: arweave gateway endpoint

--- src/environment/endpoints.ts
+++ src/environment/endpoints.ts
@@ -1,5 +1,6 @@
 export const RPC = 'https://rpc.l16.lukso.network';
 export const IPFS_GATEWAY = 'https://2eff.lukso.dev/ipfs/';
+export const ARWEAVE_GATEWAY = 'https://arweave.net/';
 export const NATIVE_TOKEN = 'LYXt';
 export const API_URL = 'https://api.lookso.io';
 export const EXPLORER_URL = 'https://explorer.execution.l16.lukso.network/';
