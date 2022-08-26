604b52a3563b92820d651de9f644dd6f976aba9b
Samuel_V <samuel@dropps.io>
2022-08-26 16:51:15 +0100

    feat: backdrop on profile dropdown

--- src/components/ProfileDropdown/ProfileDropdown.tsx
+++ src/components/ProfileDropdown/ProfileDropdown.tsx
@@ -29,6 +29,7 @@ const ProfileDropdown: FC<ProfileDropdownProps> = (props) => {
   return (
     <>
       <ActionModal open={showNoUpCloud} onClose={() => setShowNoUpCloud(false)} textToDisplay={'Unavailable (still running on L14 testnet)'} btnText={'Ok 😔'} callback={() => setShowNoUpCloud(false)}/>
+      <div className={props.showDropdown ? styles.BackDrop : ''} onClick={props.onClose}></div>
       <div className={`${styles.ProfileDropdown} ${!props.showDropdown ? styles.InactiveDropdown : ''}`}>
         <div className={styles.DropdownHeader}>
           {
