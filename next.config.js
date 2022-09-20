d05730c2b396b0c755b4e1b8e6b547c2c2ddc91b
samuel-videau <samuel.videau@efrei.net>
2022-09-20 16:49:59 +0100

    chore: environments staging and prod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+require('dotenv').config({ path: `./.env.${process.env.ENVIRONMENT}` });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    // Reference a variable that was defined in the .env.* file and make it available at Build Time
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+  },
 }
 
 module.exports = nextConfig
--- src/environment/endpoints.ts
+++ src/environment/endpoints.ts
@@ -1,8 +1,34 @@
-export const RPC = 'https://rpc.l16.lukso.network';
-export const IPFS_GATEWAY = 'https://2eff.lukso.dev/ipfs/';
-export const ARWEAVE_GATEWAY = 'https://arweave.net/';
-export const NATIVE_TOKEN = 'LYXt';
-export const API_URL = 'https://api.lookso.io';
-export const EXPLORER_URL = 'https://explorer.execution.l16.lukso.network/';
-export const POST_VALIDATOR_ADDRESS = '0xCF3f83D711844E72947172Ebe8EadAF92900B146';
-export const WEBSITE_URL = 'https://lookso.io';
+let RPC: string, NATIVE_TOKEN: string, API_URL: string, EXPLORER_URL: string, POST_VALIDATOR_ADDRESS: string, WEBSITE_URL: string;
+switch (process.env.NEXT_PUBLIC_ENV) {
+  case 'staging':
+
+    RPC = process.env.NEXT_PUBLIC_STAGING_RPC as string;
+    NATIVE_TOKEN = process.env.NEXT_PUBLIC_STAGING_NATIVE_TOKEN as string;
+    API_URL = process.env.NEXT_PUBLIC_STAGING_API_URL as string;
+    EXPLORER_URL = process.env.NEXT_PUBLIC_STAGING_EXPLORER_URL as string;
+    POST_VALIDATOR_ADDRESS = process.env.NEXT_PUBLIC_STAGING_POST_VALIDATOR_ADDRESS as string;
+    WEBSITE_URL = process.env.NEXT_PUBLIC_STAGING_WEBSITE_URL as string;
+
+    break;
+  case 'prod':
+
+    RPC = process.env.NEXT_PUBLIC_PROD_RPC as string;
+    NATIVE_TOKEN = process.env.NEXT_PUBLIC_PROD_NATIVE_TOKEN as string;
+    API_URL = process.env.NEXT_PUBLIC_PROD_API_URL as string;
+    EXPLORER_URL = process.env.NEXT_PUBLIC_PROD_EXPLORER_URL as string;
+    POST_VALIDATOR_ADDRESS = process.env.NEXT_PUBLIC_PROD_POST_VALIDATOR_ADDRESS as string;
+    WEBSITE_URL = process.env.NEXT_PUBLIC_PROD_WEBSITE_URL as string;
+
+    break;
+  default:
+
+    RPC = process.env.NEXT_PUBLIC_RPC as string;
+    NATIVE_TOKEN = process.env.NEXT_PUBLIC_NATIVE_TOKEN as string;
+    API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+    EXPLORER_URL = process.env.NEXT_PUBLIC_EXPLORER_URL as string;
+    POST_VALIDATOR_ADDRESS = process.env.NEXT_PUBLIC_POST_VALIDATOR_ADDRESS as string;
+    WEBSITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL as string;
+
+    break;
+}
+export {RPC, NATIVE_TOKEN, API_URL, EXPLORER_URL, POST_VALIDATOR_ADDRESS, WEBSITE_URL};
