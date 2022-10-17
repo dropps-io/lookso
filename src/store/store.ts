9c5ae0c8efaf061e76a7f5c5e0b9c5bb74332ad8
samuel-videau <samuel.videau@efrei.net>
2022-10-17 15:05:46 +0100

    3 feed reducers with the same structure

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import web3Reducer from './web3-reducer';
 import profileReducer from './profile-reducer';
-import feedReducer from './feed-reducer';
+import userFeedReducer, {userFeedSlice} from "./feed-reducers/user-feed-reducer";
+import profileFeedReducer, {profileFeedSlice} from "./feed-reducers/profile-feed-reducer";
+import exploreFeedReducer, {exploreFeedSlice} from "./feed-reducers/explore-feed-reducer";
 
 export const store = configureStore({
     reducer: {
         web3: web3Reducer,
         profile: profileReducer,
-        feed: feedReducer
+        userFeed: userFeedReducer,
+        exploreFeed: exploreFeedReducer,
+        profileFeed: profileFeedReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
@@ -18,3 +22,25 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export function getFeedActions(type: 'Explore' | 'Profile' | 'Feed') {
+    switch (type) {
+        case "Explore":
+            return exploreFeedSlice.actions;
+        case "Feed":
+            return userFeedSlice.actions;
+        case "Profile":
+            return profileFeedSlice.actions;
+    }
+}
+
+export const getReduxFeedState = (store: RootState ,type: 'Explore' | 'Profile' | 'Feed') => {
+    switch (type) {
+        case "Explore":
+            return store.exploreFeed;
+        case "Feed":
+            return store.userFeed;
+        case "Profile":
+            return store.profileFeed;
+    }
+}
