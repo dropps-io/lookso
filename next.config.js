a5ea18fb4e8cde55e5b7d8833a2f1900b77e933a
samuel-videau <samuel.videau@efrei.net>
2023-01-01 12:12:11 +0000

    fix: nest config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: `./.env.${process.env.ENVIRONMENT}` });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   env: {
     // Reference a variable that was defined in the .env.* file and make it available at Build Time
     NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
